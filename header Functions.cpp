8eed028e631ac81a4fc379a032331b9db41ff4d9
Carl-09 <gamenba41@gmail.com>
2024-08-23 07:10:04 -0700

    Create header Functions.cpp

---
+++ header Functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <direct.h>
+#include <dos.h>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+void list_files();
+void create_directory();
+void change_directory();
+void display_menu();
